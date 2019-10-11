ce02303bf39a4bc262ef1433d0f356acf2c23c41
musa ngcobo <ngcobomusa950@gmail.com>
2019-10-11 14:15:59 +0200

    Updates

---
+++ precache-manifest.40f34b34cb9547e6a7bd9b72a5ab33dc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a25182efb5cdc461c9ef48460b3b19",
+    "url": "/Online/index.html"
+  },
+  {
+    "revision": "47671a44f85c359fb109",
+    "url": "/Online/static/css/2.d729d116.chunk.css"
+  },
+  {
+    "revision": "0c8b3424eb43a03f34ad",
+    "url": "/Online/static/css/main.40dfd088.chunk.css"
+  },
+  {
+    "revision": "47671a44f85c359fb109",
+    "url": "/Online/static/js/2.f1c0e94c.chunk.js"
+  },
+  {
+    "revision": "0c8b3424eb43a03f34ad",
+    "url": "/Online/static/js/main.cedbbd65.chunk.js"
+  },
+  {
+    "revision": "3c1f23a53726cfb078fd",
+    "url": "/Online/static/js/runtime~main.fa48c9da.js"
+  },
+  {
+    "revision": "1850796c8658f95861e9a1a48b86de60",
+    "url": "/Online/static/media/Musa.1850796c.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Online/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Online/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Online/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Online/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Online/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
