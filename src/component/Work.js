1a479bf86213bb65ccdba7cd4579893d8c11fb1a
Musa Ngcobo <53180216+Ngcobo950@users.noreply.github.com>
2021-05-28 14:16:32 +0200

    Added work experience
    CloudeOne.mobi experience

--- src/component/Work.js
+++ src/component/Work.js
@@ -5,6 +5,12 @@ function Work() {
 		<div className="right-side">
 			<h3>WORK EXPERIENCE</h3>
 			<hr id="break"/>
+			<h6>CloudeOne.mobi</h6>
+			<p>Job Title: Software Developer Intern
+				Year :August 2020 - May 2021
+				Working on a project tallorderpos.com .We were using verious stacks called React , React Hooks and Context api , Typescript/Javascript , GraphQL 
+				Material UI ,Next.js and Ant design</p>
+		
 			<h6> KGA Life Insurance</h6>
 			<p>Job Title:  IBU Branch Administrator / Sales Consultant 
 				Year:June 2015 – September 2018
@@ -13,6 +19,7 @@ function Work() {
 				Data Capturing, Sales – team leader
 				Marketing – team leader in promotions, marketing the company products via radio shows amongst others
 				Trainer – training new agents and IBU administrators</p>
+		
 				<h6>Job Title: Call Centre Agent</h6>
 				<p>Year:	November 2014 – June 2015 Duties:  inbound and outbound calls
 				Administration
